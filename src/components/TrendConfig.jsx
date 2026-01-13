1d0f845cef9b940b56d0311b21dec9fa07cb9cf1
ingdawkul <dawkul@ous-hf.no>
2026-01-13 10:43:03 +0100

    Updated min and max

--- src/components/TrendConfig.jsx
+++ src/components/TrendConfig.jsx
@@ -2,32 +2,34 @@ export const TREND_CONFIG = {
   /* =========================
      MOTOR / POSITION (unitless)
      ========================= */
-  "NDCMotor::X1 primSecDevStats": { min: -5, max: 5, warningMin: -3, warningMax: 3 },
-  "NDCMotor::X2 primSecDevStats": { min: -5, max: 5, warningMin: -3, warningMax: 3 },
-  "NDCMotor::Y1 primSecDevStats": { min: -5, max: 5, warningMin: -3, warningMax: 3 },
-  "NDCMotor::Y2 primSecDevStats": { min: -5, max: 5, warningMin: -3, warningMax: 3 },
+  "NDCMotor::X1 primSecDevStats": { min: -0.5 , max: 0.5, warningMin: -0.25, warningMax: 0.25 },
+  "NDCMotor::X2 primSecDevStats": { min: -0.5, max: 0.5, warningMin: -0.25, warningMax: 0.25 },
+  "NDCMotor::Y1 primSecDevStats": { min: -0.5, max: 0.5, warningMin: -0.25, warningMax: 0.25 },
+  "NDCMotor::Y2 primSecDevStats": { min: -0.5, max: 0.5, warningMin: -0.25, warningMax: 0.25 },
 
-  "NDCMotor::KVBladeX1 primSecDevStats": { min: -5, max: 5, warningMin: -3, warningMax: 3 },
-  "NDCMotor::KVBladeX2 primSecDevStats": { min: -5, max: 5, warningMin: -3, warningMax: 3 },
-  "NDCMotor::KVBladeY1 primSecDevStats": { min: -5, max: 5, warningMin: -3, warningMax: 3 },
-  "NDCMotor::KVBladeY2 primSecDevStats": { min: -5, max: 5, warningMin: -3, warningMax: 3 },
+  "NDCMotor::KVBladeX1 primSecDevStats": { min: -0.03, max: 0.03, warningMin: -0.03, warningMax: 0.03 },    
+  "NDCMotor::KVBladeX2 primSecDevStats": { min: -0.03, max: 0.03, warningMin: -0.03, warningMax: 0.03 },
+  "NDCMotor::KVBladeY1 primSecDevStats": { min: -0.03, max: 0.03, warningMin: -0.03, warningMax: 0.03 },
+  "NDCMotor::KVBladeY2 primSecDevStats": { min: -0.03, max: 0.03, warningMin: -0.03, warningMax: 0.03 },
 
-  "NDCMotor::KVFilterFoil primSecDevStats": { min: -5, max: 5, warningMin: -3, warningMax: 3 },
-  "NDCMotor::KVFilterShape primSecDevStats": { min: -5, max: 5, warningMin: -3, warningMax: 3 },
+  "NDCMotor::KVFilterFoil primSecDevStats": { max: 0.07, warningMax: 0.05 },
+  "NDCMotor::KVFilterShape primSecDevStats": { max: 0.07, warningMax: 0.05 },
 
-  "NDCMotor::PosTarget primSecDevStats": { min: -5, max: 5, warningMin: -3, warningMax: 3 },
-  "NDCMotor::PosRotation primSecDevStats": { min: -5, max: 5, warningMin: -3, warningMax: 3 },
-  "NDCMotor::PosIonChamber primSecDevStats": { min: -5, max: 5, warningMin: -3, warningMax: 3 },
-  "NDCMotor::PosY primSecDevStats": { min: -5, max: 5, warningMin: -3, warningMax: 3 },
-  "NDCMotor::PosEnergySwitch primDriftStats": { min: -5, max: 5, warningMin: -3, warningMax: 3 },
+  "NDCMotor::PosTarget primSecDevStats": { min: -0.5, max: 0.5, warningMin: -0.3, warningMax: 0.3 },
+  "NDCMotor::PosRotation primSecDevStats": { min: -0.75, max: 0.075, warningMin: -0.05, warningMax: 0.05 },
+  "NDCMotor::PosIonChamber primSecDevStats": { min: -0.25, max: 0.25, warningMin: -0.2, warningMax: 0.2 },
+  "NDCMotor::PosY primSecDevStats": { min: -0.5, max: 0.5, warningMin: -0.25, warningMax: 0.25 },
+  "NDCMotor::PosEnergySwitch primDriftStats": { min: -0.005, max: 0.005, warningMin: -0.003, warningMax: 0.003 },
 
   "MLCController::logStatistics MLCCarriage_BankA_primSecDevStats": {
+    unit: "mm",
     min: -0.06,
     max: 0.06,
     warningMin: -0.05,
     warningMax: 0.05
   },
   "MLCController::logStatistics MLCCarriage_BankB_primSecDevStats": {
+    unit: "mm",
     min: -0.06,
     max: 0.06,
     warningMin: -0.05,
@@ -39,10 +41,10 @@ export const TREND_CONFIG = {
      ========================= */
   "STNSF6GasCtrl::logStatistics SF6GaswaveGuidePressureStatistics": {
     unit: "bar",
-    min: 28.6,
+    min: 27,
     max: 33.0,
-    warningMin: 29.0,
-    warningMax: 32.8
+    warningMin: 28.5,
+    warningMax: 32.5
   },
 
   /* =========================
@@ -50,108 +52,126 @@ export const TREND_CONFIG = {
      ========================= */
   "BGMSubNodeCntrl::logStatistics EGN_boardTemperature": {
     unit: "°C",
-    max: 70,
-    warningMax: 60
+    min: 38,
+    max: 52,
+    warningMin: 40,
+    warningMax: 50
   },
 
   "STNPwrHandlerBase::logStatistics PowerAPD_Temperature": {
     unit: "°C",
-    max: 75,
-    warningMax: 65
+    max: 48,
+    min: 25,
+    warningMax: 46,
+    warningMin: 28
   },
   "STNPwrHandlerBase::logStatistics PowerSPD_Temperature": {
     unit: "°C",
-    max: 75,
-    warningMax: 65
+    max: 48,
+    min: 25,
+    warningMin: 28,
+    warningMax: 40
   },
   "STNPwrHandlerBase::logStatistics PowerGPD_Temperature": {
     unit: "°C",
-    max: 75,
-    warningMax: 65
+    max: 48,
+    min: 30,
+    warningMin: 35,
+    warningMax: 46
   },
 
   /* =========================
      FANS
      ========================= */
   "STNPwrHandlerBase::logStatistics GPD_ACFanStatistics": {
-    min: 0.3,
-    warningMin: 0.2
+    max: 0.5,
+    min: 0,
+    warningMin: 0.3,
+    warningMax: 0.4
   },
   "STNPwrHandlerBase::logStatistics SPD_ACFanStatistics": {
-    min: 0.3,
-    warningMin: 0.2
+    min: 0.2,
+    max: 0.8,
+    warningMax: 0.6,
+    warningMin: 0.4
   },
 
   /* =========================
      COOLING / FLOW / TEMP
      ========================= */
   "STNCoolingCtrl::logStatistics CoolingbendMagFlowHighStatistics": {
-    unit: "l/min",
-    min: 0.48,
-    max: 0.8,
+    unit: "gpm",
+    min: 0.4,
+    max: 1.0,
     warningMin: 0.5,
-    warningMax: 0.78
+    warningMax: 0.8
   },
   "STNCoolingCtrl::logStatistics CoolingcityWaterFlowHighStatistics": {
-    unit: "l/min",
-    min: 4,
-    max: 5,
-    warningMin: 4.1,
-    warningMax: 4.9
+    unit: "gpm",
+    min: 2,
+    max: 8,
+    warningMin: 2.5,
+    warningMax: 6.0
   },
   "STNCoolingCtrl::logStatistics CoolingcityWaterTempStatistics": {
     unit: "°C",
-    min: 8,
-    max: 40,
-    warningMin: 10,
-    warningMax: 35
+    min: 10,
+    max: 16,
+    warningMin: 11,
+    warningMax: 14
   },
   "STNCoolingCtrl::logStatistics CoolingguideFlowFlowHighStatistics": {
-    unit: "l/min",
-    min: 0.48,
-    max: 0.8,
-    warningMin: 0.5,
-    warningMax: 0.78
+    unit: "gpm",
+    min: 2.5,
+    max: 6,
+    warningMax: 4,
+    warningMin: 3
   },
   "STNCoolingCtrl::logStatistics CoolingklystronFlowHighStatistics": {
-    unit: "l/min",
-    min: 0.48,
-    max: 0.8,
-    warningMin: 0.5,
-    warningMax: 0.78
+    unit: "gpm",
+    max: 8,
+    min: 5,
+    warningMax: 7,
+    warningMin: 5.5
   },
   "STNCoolingCtrl::logStatistics CoolingklystronSolenoidFlowHighStatistics": {
-    unit: "l/min",
-    min: 6,
-    warningMin: 8
+    unit: "gpm",
+    max: 6,
+    min: 3,
+    warningMin: 4,
+    warningMax: 5
   },
   "STNCoolingCtrl::logStatistics CoolingprimaryCollimatorFlowHighStatistics": {
-    unit: "l/min",
-    min: 6,
-    warningMin: 8
+    unit: "gpm",
+    min: 1.0,
+    max: 1.8,
+    warningMin: 1.2,
+    warningMax: 1.6
   },
   "STNCoolingCtrl::logStatistics CoolingpumpOutletTempStatistics": {
     unit: "°C",
-    max: 40,
-    warningMax: 35
+    max: 45,
+    warningMax: 42,
+    warningMin: 38
   },
   "STNCoolingCtrl::logStatistics CoolingtankInputTempStatistics": {
     unit: "°C",
-    max: 35,
-    warningMax: 30
+    max: 48,
+    warningMax: 45,
+    warningMin: 38
   },
   "STNCoolingCtrl::logStatistics CoolingtargetFlowHighStatistics": {
-    unit: "l/min",
-    min: 0.48,
-    max: 0.8,
-    warningMin: 0.5,
-    warningMax: 0.78
+    unit: "gpm ",
+    min: 0.8,
+    max: 1.4,
+    warningMin: 0.9,
+    warningMax: 1.2
   },
   "STNCoolingCtrl::logStatistics SlimcombineGuideSolenoidFlowHighStatistics": {
-    unit: "l/min",
-    min: 0.48,
-    max: 0.8,
-    warningMin: 0.5,
-    warningMax: 0.78
+    unit: "gpm",
+    min: 3.0,
+    max: 4.0,
+    warningMin: 3.2,
+    warningMax: 3.8
   }
 };
